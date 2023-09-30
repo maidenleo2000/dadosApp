347e4ff3eebff8cf99991c3316f71020eb6b9ec6
Leandro Martello <maidenleo2000@gmail.com>
2023-09-30 18:57:07 -0300

    Proyecto dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dadosApp';
+  dadoIzquierda = '../assets/img/dice1.png'
+  dadoDerecha = '../assets/img/dice4.png'
+
+  numero1:number = 1;
+  numero2:number = 2;
+
+  tirarDados(): void{
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+    // console.log(this.numero1)
+    // console.log(this.numero2)
+  }
 }
